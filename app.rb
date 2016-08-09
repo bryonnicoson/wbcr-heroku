26d5e059158d8f9f9dd3265c6fee1763dcfb24c8
Bryon Nicoson <bryon.nicoson@gmail.com>
2016-08-09 10:50:52 -0500

    adding xml_to_json - first trial

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'bundler'
 Bundler.require()
 
 get '/' do
-	file = File.read('book.json')
-	hash = JSON.parse(file)
-	hash.to_json
+#	file = File.read('book.json')
+#	hash = JSON.parse(file)
+#	hash.to_json
+	xml_feed_to_json(http://api.petfinder.com/shelter.getPets?id=IL542&count=1000&key=4f968c74b90183bfb8519a8cf64844f2&callback=?)
+end
+
+def xml_feed_to_json(url)
+	@xml = HTTParty.get(url)
+	Crack::XML.parse(@xml).to_json
 end
