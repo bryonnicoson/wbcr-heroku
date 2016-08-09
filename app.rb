5c34dbc2f12824a03c48d26e6f1507db21247208
Bryon Nicoson <bryon.nicoson@gmail.com>
2016-08-09 10:53:19 -0500

    fixed quoting of url string

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ get '/' do
 #	file = File.read('book.json')
 #	hash = JSON.parse(file)
 #	hash.to_json
-	xml_feed_to_json(http://api.petfinder.com/shelter.getPets?id=IL542&count=1000&key=4f968c74b90183bfb8519a8cf64844f2&callback=?)
+	xml_feed_to_json('http://api.petfinder.com/shelter.getPets?id=IL542&count=1000&key=4f968c74b90183bfb8519a8cf64844f2&callback=?')
 end
 
 def xml_feed_to_json(url)
