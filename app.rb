144bc278e57416f26c907a8160dc87f1ad594c34
Bryon Nicoson <bryon.nicoson@gmail.com>
2016-08-09 13:34:18 -0500

    pushing test limit to 10

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'bundler'
 Bundler.require()
 
 get '/' do
-	xml_feed_to_json('http://api.petfinder.com/shelter.getPets?id=IL542&count=1&key=4f968c74b90183bfb8519a8cf64844f2&callback=?')
+	xml_feed_to_json('http://api.petfinder.com/shelter.getPets?id=IL542&count=10&key=4f968c74b90183bfb8519a8cf64844f2&callback=?')
 end
 
 def xml_feed_to_json(url)
